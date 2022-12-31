b7ff6c7d967228468ab4a62971aeac3a057a9907
immhusnainali <m.husnainali444@gmail.com>
2022-12-31 23:17:23 +0500

    v 1.1.0

--- propertydetail.php
+++ propertydetail.php
@@ -208,22 +208,7 @@ include("config.php");
 					
                     <div class="col-lg-4">
       
-                        <h4 class="double-down-line-left text-secondary position-relative pb-4 mb-4 mt-5">Featured Property</h4>
-                        <ul class="property_list_widget">
-							
-                            <?php 
-                            $query=mysqli_query($con,"SELECT * FROM `property` WHERE isFeatured = 1 ORDER BY date DESC LIMIT 3");
-                                    while($row=mysqli_fetch_array($query))
-                                    {
-                            ?>
-                            <li> <img src="admin/property/<?php echo $row['18'];?>" alt="pimage">
-                                <h6 class="text-secondary hover-text-success text-capitalize"><a href="propertydetail.php?pid=<?php echo $row['0'];?>"><?php echo $row['1'];?></a></h6>
-                                <span class="font-14"><i class="fas fa-map-marker-alt icon-success icon-small"></i> <?php echo $row['14'];?></span>
-                                
-                            </li>
-                            <?php } ?>
-
-                        </ul>
+                        
 
                         <div class="sidebar-widget mt-5">
                             <h4 class="double-down-line-left text-secondary position-relative pb-4 mb-4">Recently Added Property</h4>
