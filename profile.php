f828fdd6fec26beaa37b131b16d17f2bfde6721b
immhusnainali <m.husnainali444@gmail.com>
2023-01-06 07:32:39 +0500

    minnor change 1.1

--- profile.php
+++ profile.php
@@ -172,7 +172,7 @@ if(isset($_POST['insert']))
     </div>
 </div>
 <!-- Wrapper End --> 
-<!-- FOR MORE PROJECTS visit: freeprojectscodes.com -->
+
 <!--	Js Link
 ============================================================--> 
 <script src="js/jquery.min.js"></script> 
